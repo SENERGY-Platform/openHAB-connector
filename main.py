c0d3a47930bc4c813bd0dd6a212fde9cc9eaafe2
Hannes <mail@hannesh.de>
2018-03-26 14:00:45 +0200

    add conf

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import os,sys,inspect
-import_path = os.path.realpath(os.path.abspath(os.path.join(os.path.split(inspect.getfile(inspect.currentframe()))[0],"connector_client")))
+import_path = os.path.realpath(os.path.abspath(os.path.join(os.path.split(inspect.getfile(inspect.currentframe()))[0],"connector-client")))
 if import_path not in sys.path:
     sys.path.insert(0, import_path)
 
