0ab1083e60c8ab970c48c7078a99b249fb6936f1
Hannes <mail@hannesh.de>
2018-03-26 13:59:53 +0200

    add conf

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ import_path = os.path.realpath(os.path.abspath(os.path.join(os.path.split(inspec
 if import_path not in sys.path:
     sys.path.insert(0, import_path)
 
-from connector.client import Client
-from connector.modules import DevicePool
+from connector import client
+from connector import modules
 from monitor import monitor
 from observer import observer
 from executer import executer
@@ -12,7 +12,7 @@ from status_pinger import pinger
 
 
 if __name__ == "__main__":
-    connector_client = Client(device_manager=DevicePool) 
+    connector_client = client.Client(device_manager=modules.DevicePool) 
     
     monitor_openhab = monitor.Monitor()
     monitor_openhab.start()
