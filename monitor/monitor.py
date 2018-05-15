302c70dcd9981bd8812195342bd3b6d771290669
Hannes <mail@hannesh.de>
2018-05-15 12:52:03 +0200

    updated monitor

--- monitor/monitor.py
+++ monitor/monitor.py
@@ -220,12 +220,6 @@ class Monitor(threading.Thread):
         # 3. Compare services
         if len(services) == 0:
             return (True, found_device_type_id)
-        elif device_types_with_same_name["Exists"]:
-            return (True, device_types_with_same_name["Id"])
-        else:
-            return (False, None)
-        
-        """
         elif not device_types_with_same_name["Exists"]:
             return (False, None)
         else:
@@ -244,7 +238,6 @@ class Monitor(threading.Thread):
             return (True, found_device_type_id)
         else:
             return (False, found_device_type_id)
-        """
 
     def get_platform_data_type(self, item_type, thing_is_sensor):
         """
