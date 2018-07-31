ef062bfb90eb7a9c11cd517afdcc449b0be555c0
y-du <42994541+y-du@users.noreply.github.com>
2018-07-31 13:39:14 +0200

    added central logger for openhab client

---
+++ logger/logger.py
@@ -0,0 +1,10 @@
+try:
+    from connector_client.modules.logger import connector_client_log_handler
+except ImportError as ex:
+    exit("{} - {}".format(__name__, ex.msg))
+import logging
+
+
+root_logger = logging.getLogger('openhab')
+root_logger.setLevel(logging.INFO)
+root_logger.addHandler(connector_client_log_handler)
